51e3b7f39ecd0db76d68fdb6f82eae45691d4619
Diego G <d.g.g.87@hotmail.com>
2023-11-23 09:08:29 -0600

    params

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
-app.use(express.static('./public'));
+const {products} = require('./data');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+  res.send('<h1>Home page</h1><a href="/api/products">products</a>');
+});
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const {id, name, image} = product;
+    return {id, name, image};
+  });
+  res.json(newProducts);
 });
 
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>Resource not found</h1>');
+app.get('/api/products/:productID', (req, res) => {
+  // console.log(req);
+  // console.log(req.params);
+  const {productID} = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send('Product does not exist');
+  }
+
+  return res.json(singleProduct);
+});
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.params);
+  res.send('Hello World');
 });
 
 app.listen(5000, () => {
