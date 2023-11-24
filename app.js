2dcadd0f9de24a151f5966d4d5ab87b3d069055c
Diego G <d.g.g.87@hotmail.com>
2023-11-24 09:29:19 -0600

    middlewares

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
 const express = require('express');
 const app = express();
-const {products} = require('./data');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Home page</h1><a href="/api/products">products</a>');
-});
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const {id, name, image} = product;
-    return {id, name, image};
-  });
-  res.json(newProducts);
-});
+// res => middleware => res
 
-app.get('/api/products/:productID', (req, res) => {
-  // console.log(req);
-  // console.log(req.params);
-  const {productID} = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send('Product does not exist');
-  }
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
 
-  return res.json(singleProduct);
+app.get('/', logger, (req, res) => {
+  res.send('Home');
 });
 
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params);
-  res.send('Hello World');
+app.get('/about', logger, (req, res) => {
+  res.send('About');
 });
 
 app.listen(5000, () => {
