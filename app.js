b5bc4182f6373eb4ef67b86f123370273b41853e
Diego G <d.g.g.87@hotmail.com>
2023-11-24 18:03:30 -0600

    simple example GET method

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
 const express = require('express');
 const app = express();
-const logger = require('./logger');
-const authorize = require('./authorize');
-const morgan = require('morgan');
+let {people} = require('./data');
 
-// res => middleware => res
-// app.use([logger, authorize]);
-
-// app.use(express.static('./public'));
-app.use(morgan('tiny'));
-
-app.get('/', (req, res) => {
-  res.send('Home');
-});
-
-app.get('/about', (req, res) => {
-  res.send('About');
-});
-app.get('/api/products', (req, res) => {
-  res.send('products');
-});
-app.get('/api/items', (req, res) => {
-  console.log(req.user);
-  res.send('items');
+app.get('/api/people', (req, res) => {
+  res.status(200).json({success: true, data: people});
 });
 
 app.listen(5000, () => {
